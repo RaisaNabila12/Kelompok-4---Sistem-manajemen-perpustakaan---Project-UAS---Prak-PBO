d7f96861b54de0967c79157eda68ddc3f16c242f
raseuki06 <muhammadraseuki864@gmail.com>
2025-11-24 23:01:10 +0700

    Update Pengguna.java

--- bin/src/model/Pengguna.java
+++ bin/src/model/Pengguna.java
@@ -1,1 +1,10 @@
+package model;
 
+/**
+ * Kelas abstrak yang merepresentasikan pengguna sistem (Admin atau Anggota).
+ */
+public abstract class Pengguna {
+    private String idPengguna;
+    private String nama;
+    private String username;
+    private String password;
